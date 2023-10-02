efcbbf113c24fda11b7916cd26dffc151286e94f
AmerDje <amer.dje10n20@gmail.com>
2023-10-02 18:52:14 +0100

    fix issues

--- lib/core/utils/extensions/navigation_extension.dart
+++ lib/core/utils/extensions/navigation_extension.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 
 extension NavigationExtension on BuildContext {
   go(Widget widget, RouteSettings settings) {
-    Navigator.push(this, MaterialPageRoute(builder: (_) {
-      return widget;
-    }));
+    Navigator.push(
+        this,
+        MaterialPageRoute(
+            builder: (_) {
+              return widget;
+            },
+            settings: settings));
   }
 }
