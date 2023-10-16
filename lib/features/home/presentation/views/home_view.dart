4ebe83a0323bac27a1dd661313044ddb32c5aab7
AmerDje <amer.dje10n20@gmail.com>
2023-10-16 18:02:40 +0100

    refactor code

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -7,28 +7,30 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset:
-            false, //to fix the floating action button when the keyboard opened
-        floatingActionButton: FloatingActionButton(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(15))),
-          onPressed: () {
-            showModalBottomSheet<void>(
-                isScrollControlled:
-                    true, // sets the bottom sheet to full height
-                isDismissible: true, //to dismiss when hold and drag to button
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        topRight: Radius.circular(16))),
-                context: context,
-                builder: (BuildContext context) {
-                  return const NoteBottomSheet();
-                });
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: const SafeArea(child: HomeViewBody()));
+    return SafeArea(
+      child: Scaffold(
+          resizeToAvoidBottomInset:
+              false, //to fix the floating action button when the keyboard opened
+          floatingActionButton: FloatingActionButton(
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(15))),
+            onPressed: () {
+              showModalBottomSheet<void>(
+                  isScrollControlled:
+                      true, // sets the bottom sheet to full height
+                  isDismissible: true, //to dismiss when hold and drag to button
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(16),
+                          topRight: Radius.circular(16))),
+                  context: context,
+                  builder: (BuildContext context) {
+                    return const NoteBottomSheet();
+                  });
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: const HomeViewBody()),
+    );
   }
 }
